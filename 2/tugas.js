a5d597f7e2401856cbd907d84a68c284cae51c6c
Supardi G <pardiwan267@gmail.com>
2023-10-04 11:07:34 +0700

    tambah tugas 2

--- 2/tugas.js
+++ 2/tugas.js
@@ -1,20 +1,9 @@
-const mahasiswa = {
-  nama: "Supardi G",
-  kelas: "TI22H",
-  nim: 20220040084,
-};
+const firstName = "Supardi";
+const lastName = "Gunawan";
+const age = 20;
+const isMarried = false;
 
-const nilai = {
-  imk: 80,
-  rpl: 100,
-};
-
-console.log("Nama: ", mahasiswa.nama);
-console.log("Kelas: ", mahasiswa.kelas);
-console.log("NIM: ", mahasiswa.nim);
-console.log("\nNilai");
-console.log("Interaksi Manusia dan komputer: ", nilai.imk);
-console.log("Rekayasa Perangkat Lunak: ", nilai.rpl);
-
-const rataRata = (nilai.imk + nilai.rpl) / 2;
-console.log("Rata-rata nilai: ", rataRata);
+console.log("First Name: ", firstName);
+console.log("Last Name: ", lastName);
+console.log("Age: ", age);
+console.log("Married: ", isMarried ? "Yes" : "No");
