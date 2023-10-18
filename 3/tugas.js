c48c79c5f33c055051d46bc2c853795d82e53605
Supardi G <pardiwan267@gmail.com>
2023-10-19 02:22:30 +0700

    tambah tugas sesi 3

--- 3/tugas.js
+++ 3/tugas.js
@@ -1,14 +1,9 @@
-const mahasiswa = {
-  nim: 20220040084,
-  nama: "Supardi G",
-  prodi: "Teknik Informatika",
-  alamat: "Cianjur",
-  ttl: "Cianjur, 26 Juli 2003",
-};
-
-const { nim, nama, prodi, alamat, ttl } = mahasiswa;
-console.log("NIM: ", nim);
-console.log("Nama: ", nama);
-console.log("Prodi: ", prodi);
-console.log("Alamat: ", alamat);
-console.log("Tempat, tanggal lahir: ", ttl);
+const restaurant = [
+  {
+    name: "Supardi's restaurant",
+    city: "Cianjur",
+    "favorite drink": "Power F",
+    "favorite food": "Seblak",
+    isVegan: false,
+  },
+];
